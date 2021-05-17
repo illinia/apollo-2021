44d7f42b5ab8bdaaac3c008eec1a2781f7b0eee0
illinia <67290425+illinia@users.noreply.github.com>
2021-05-17 23:04:59 +0900

    apollo study

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom';
+import App from './components/App';
+import client from './apollo';
+import { ApolloProvider } from '@apollo/client/react'
 
 ReactDOM.render(
-
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ src/routes/Detail.js
@@ -0,0 +1,28 @@
+import { useParams } from 'react-router-dom';
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/client';
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id){
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`
+
+export default () => {
+  const { id } = useParams()
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: +id }
+  })
+  if (loading) {
+    return "loading"
+  }
+  if (data && data.movie) {
+    console.log(loading, data)
+    return data.movie.title
+  }
+}
