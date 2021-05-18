f3803c989f4e0e106c5261265fc5fcc7347ca0bc
illinia <67290425+illinia@users.noreply.github.com>
2021-05-18 22:32:24 +0900

    apollo finish

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,27 @@
-import ApolloClient from 'apollo-boost'
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/"
-})
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+  resolvers: {
+    Movie: {
+      isLiked: () => false, // 디폴트로 false 값 지니도록.
+    },
+    Mutation: {
+      toggleLikeMovie: (_, { id, isLiked }, { cache }) => {
+        cache.modify({
+          id: `Movie:${id}`,
+          fields: {
+            isLiked: (isLiked) => !isLiked, // 현재 isLiked 값을 받아서 true-false 토글
+          },
+        });
+      },
+      unlikeMovie: (_, { id }, { cache }) => {
+        cache.writeData({
 
+        })
+      }
+    },
+  },
+});
 export default client;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
@@ -9,6 +10,7 @@ const GET_MOVIES = gql`
     movies {
       id
       medium_cover_image
+      isLiked @client
     }
   }
 `
@@ -44,6 +46,15 @@ const Loading = styled.div`
   margin-top: 10px;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 export default () => {
   const { loading, data } = useQuery(GET_MOVIES)
   return (
@@ -53,9 +64,12 @@ export default () => {
         <Subtitle>I love GraphQL</Subtitle>
       </Header>
       {loading && <Loading>Loading...</Loading>}
-      {!loading &&
-        data.movies &&
-        data.movies.map(m => <Movie key={m.id} id={m.id} />)}
+      <Movies>
+        {data?.movies?.map(m => (
+          <Movie
+            key={m.id} id={m.id} isLiked={m.isLiked} bg={m.medium_cover_image} />
+        ))}
+      </Movies>
     </Container>
   )
 }
